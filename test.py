360d250c784719790d14c032b56da250e248d102
Tim Poerschke <info@coder-tim.de>
2021-10-30 11:20:57 +0200

    Plotting generischer implementiert

--- test.py
+++ test.py
@@ -6,77 +6,70 @@ import fuzzy
 m1 = fuzzy.M1(800, 1400) # Geringe Höhe
 m2 = fuzzy.M2(900, 2000) # Mittlere Höhe
 m3 = fuzzy.M3(1400, 2000) # Große Höhe
+input_sets = [m1, m2, m3]
 
 a1 = fuzzy.M1(12, 65) # Viel Bremskraft
 a2 = fuzzy.M2(30, 90) # Mittlere Bremskraft
 a3 = fuzzy.M3(60, 70) # Wenig Bremskraft
+output_sets = [a1, a2, a3]
 
-agg = fuzzy.Aggregator([m1, m2, m3], [a1, a2, a3], output_upper_bound=100)
+agg = fuzzy.Aggregator(input_sets, [a1, a2, a3], output_upper_bound=100)
 # x_val = 1200 # 200, 800, 1200, 1800
 # (xx, yy), (inf1, inf2, inf3) = agg.aggregate(x_val)
 
 
-# Eingabe- und Ausgabemengen plotten
-mx1, my1 = [], []
-mx2, my2 = [], []
-mx3, my3 = [], []
-ax1, ay1 = [], []
-ax2, ay2 = [], []
-ax3, ay3 = [], []
+# Plotten der Eingabe- und Ausgabemengen vorbereiten
+input_sets_x = [[] for _ in input_sets]
+input_sets_y = [[] for _ in input_sets]
+output_sets_x = [[] for _ in input_sets]
+output_sets_y = [[] for _ in input_sets]
+
+for i in range(0, 2000+1):
+    # Eingabemengen
+    for j in range(0, len(input_sets)):
+        input_sets_x[j].append(i)
+        input_sets_y[j].append(input_sets[j](i))
+
+for i in range(0, 100):
+    # Ausgabemengen
+    for j in range(0, len(output_sets)):
+        output_sets_x[j].append(i)
+        output_sets_y[j].append(output_sets[j](i))
 
 plt.ion()
 fig, (axis1, axis2) = plt.subplots(1, 2)
 
-for i in range(0, 2000+1):
-    # Eingabemengen
-    mx1.append(i)
-    my1.append(m1(i))
-    mx2.append(i)
-    my2.append(m2(i))
-    mx3.append(i)
-    my3.append(m3(i))
+colors = ["red", "blue", "green"]
 
-for i in range(0, 100):
-     # Ausgabemengen
-    ax1.append(i)
-    ay1.append(a1(i))
-    ax2.append(i)
-    ay2.append(a2(i))
-    ax3.append(i)
-    ay3.append(a3(i))
-
-for x_val in range(0, 2000, 10):
+for x_val in range(2000, -1, -10):
     #x_val = 1200 # 200, 800, 1200, 1800
-    (xx, yy), (inf1, inf2, inf3) = agg.aggregate(x_val)
+    (xx, yy), infs = agg.aggregate(x_val)
 
     axis1.clear()
     axis1.set_title("Eingabemengen")
-    axis1.vlines(x_val, 0, max(inf1, inf2, inf3), color="gray", linestyle="dotted")
-    axis1.plot(mx1, my1, color="red")
-    axis1.hlines(inf1, x_val, 2000, color="red", linestyle="dotted")
-    axis1.plot(mx2, my2, color="blue")
-    axis1.hlines(inf2, x_val, 2000, color="blue", linestyle="dotted")
-    axis1.plot(mx3, my3, color="green")
-    axis1.hlines(inf3, x_val, 2000, color="green", linestyle="dotted")
+    axis1.vlines(x_val, 0, max(infs), color="dimgray", linestyle="dotted")
+    for i in range(0, len(input_sets)):
+        axis1.plot(input_sets_x[i], input_sets_y[i], color=colors[i])
+        axis1.hlines(infs[i], x_val, 2000, color=colors[i], linestyle="dotted")
 
     axis2.clear()
     axis2.set_title("Ausgabemengen")
-    axis2.plot(ax1, ay1, color="red")
-    axis2.hlines(inf1, 0, 100, color="red", linestyle="dotted")
-    axis2.plot(ax2, ay2, color="blue")
-    axis2.hlines(inf2, 0, 100, color="blue", linestyle="dotted")
-    axis2.plot(ax3, ay3, color="green")
-    axis2.hlines(inf3, 0, 100, color="green", linestyle="dotted")
+    for i in range(0, len(input_sets)):
+        axis2.plot(output_sets_x[i], output_sets_y[i], color=colors[i])
+        axis2.hlines(infs[i], 0, 100, color=colors[i], linestyle="dotted")
 
     axis2.axes.get_yaxis().set_ticks([])
-    plt.subplots_adjust(wspace=0.05)
+    plt.subplots_adjust(wspace=0.01)
 
     # Aggregation darstellen
-    axis2.plot(xx, yy, color="black", linewidth=3)
+    axis2.plot(xx, yy, color="black", linewidth=2)
+    axis2.fill_between(xx, yy, color="gainsboro")
     # Schwerpunkt bestimmen
     centroid = fuzzy.Defuzzy().centroid(xx, yy)
-    axis2.vlines(centroid, 0, 1, color="gray", linestyle="dashed")
+    axis2.vlines(centroid, 0, 1, color="dimgray", linestyle="dashed")
+    axis2.text(centroid, -0.03, f"{centroid:.1f}")
     fig.canvas.draw()
     plt.pause(0.02)
 
-time.sleep(30)
+plt.ioff()
+plt.show()
