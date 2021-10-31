3ecd427868db4da80daccc3ff5a80fbb9863f4e1
Tim Poerschke <info@coder-tim.de>
2021-10-31 19:28:08 +0100

    Landung mit 4 Fuzzy-Mengen

--- mondlandung.py
+++ mondlandung.py
@@ -5,27 +5,45 @@ from landing_game import game_tick, game_state, set_lever, uhr
 from plotting import Plotting
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--n-sets', type=int, required=False, default=3, choices=[3, 5])
 parser.add_argument('--live-plot', action='store_const', const=True, required=False)
 parser.add_argument('--debug', action='store_const', const=True, required=False)
 args = parser.parse_args()
 
-# Fuzzy
+
 m1 = fuzzy.M1(800, 1400) # Geringe Höhe
 m2 = fuzzy.M2(900, 2000) # Mittlere Höhe
 m3 = fuzzy.M3(1400, 2000) # Große Höhe
+input_sets = (m1, m2, m3)
 
 # Mengen hier quasi verdreht, wird unten 
 # mit "100 - Ausgabe" verrechnet
 a1 = fuzzy.M1(12, 65) # Viel Bremskraft
 a2 = fuzzy.M2(30, 90) # Mittlere Bremskraft
 a3 = fuzzy.M3(60, 70) # Wenig Bremskraft
+output_sets = (a1, a2, a3)
 
-agg = fuzzy.Aggregator([m1, m2, m3], [a1, a2, a3], 100)
+if args.n_sets == 5:
+    m1 = fuzzy.M1(100, 900)   # Sehr geringe Höhe
+    m2 = fuzzy.M2(400, 1200)  # Geringe Höhe
+    m3 = fuzzy.M2(900, 1600)  # Mittlere Höhe
+    m4 = fuzzy.M2(1200, 2000) # Große Höhe
+    m5 = fuzzy.M3(1400, 2000) # Sehr große Höhe
+    input_sets = (m1, m2, m3, m4, m5)
+
+    a1 = fuzzy.M1(26, 30) # Sehr Viel Bremskraft
+    a2 = fuzzy.M2(26, 50) # Viel Bremskraft
+    a3 = fuzzy.M2(30, 60) # Mittlere Bremskraft
+    a4 = fuzzy.M2(40, 70) # Wenig Bremskraft
+    a5 = fuzzy.M3(60, 75) # Sehr Wenig Bremskraft
+    output_sets = (a1, a2, a3, a4, a5)
+
+agg = fuzzy.Aggregator(input_sets, output_sets, 100)
 defuzzy = fuzzy.Defuzzy()
 
 plotting = None
 if args.live_plot:
-    plotting = Plotting([m1, m2, m3], [a1, a2, a3], agg, defuzzy)
+    plotting = Plotting(input_sets, output_sets, agg, defuzzy)
 # Hauptschleife
 while True:
     uhr.tick(30)
--- plotting.py
+++ plotting.py
@@ -7,16 +7,31 @@ import fuzzy
 fig, (axis1, axis2) = plt.subplots(1, 2)
 plt.subplots_adjust(wspace=0.01)
 
-def init_fuzzy_system():
-    m1 = fuzzy.M1(800, 1400) # Geringe Höhe
-    m2 = fuzzy.M2(900, 2000) # Mittlere Höhe
-    m3 = fuzzy.M3(1400, 2000) # Große Höhe
-    input_sets = [m1, m2, m3]
+def init_fuzzy_system(number_of_sets):
+    if number_of_sets == 5:
+        m1 = fuzzy.M1(100, 900)   # Sehr geringe Höhe
+        m2 = fuzzy.M2(400, 1200)  # Geringe Höhe
+        m3 = fuzzy.M2(900, 1600)  # Mittlere Höhe
+        m4 = fuzzy.M2(1200, 2000) # Große Höhe
+        m5 = fuzzy.M3(1400, 2000) # Sehr große Höhe
+        input_sets = (m1, m2, m3, m4, m5)
 
-    a1 = fuzzy.M1(12, 65) # Viel Bremskraft
-    a2 = fuzzy.M2(30, 90) # Mittlere Bremskraft
-    a3 = fuzzy.M3(60, 70) # Wenig Bremskraft
-    output_sets = [a1, a2, a3]
+        a1 = fuzzy.M1(20, 30) # Sehr Viel Bremskraft
+        a2 = fuzzy.M2(20, 50) # Viel Bremskraft
+        a3 = fuzzy.M2(30, 60) # Mittlere Bremskraft
+        a4 = fuzzy.M2(40, 70) # Wenig Bremskraft
+        a5 = fuzzy.M3(60, 75) # Sehr Wenig Bremskraft
+        output_sets = (a1, a2, a3, a4, a5)
+    else:
+        m1 = fuzzy.M1(800, 1400) # Geringe Höhe
+        m2 = fuzzy.M2(900, 2000) # Mittlere Höhe
+        m3 = fuzzy.M3(1400, 2000) # Große Höhe
+        input_sets = (m1, m2, m3)
+
+        a1 = fuzzy.M1(12, 65) # Viel Bremskraft
+        a2 = fuzzy.M2(30, 90) # Mittlere Bremskraft
+        a3 = fuzzy.M3(60, 70) # Wenig Bremskraft
+        output_sets = (a1, a2, a3)
 
     agg = fuzzy.Aggregator(input_sets, output_sets, output_upper_bound=100)
     return (input_sets, output_sets), agg
@@ -45,7 +60,7 @@ def init_set_values(input_sets, output_sets):
 
 
 def plot_all(input_sets_x, input_sets_y, output_sets_x, output_sets_y, agg_x, agg_y, infs, x_val, centroid):
-    colors = ["red", "blue", "green"]
+    colors = ["tab:red", "tab:blue", "tab:green", "tab:purple", "tab:orange"]
     # Eingabemengen darstellen
     axis1.clear()
     axis1.set_title("Eingabemengen")
@@ -71,10 +86,10 @@ def plot_all(input_sets_x, input_sets_y, output_sets_x, output_sets_y, agg_x, ag
     axis2.text(centroid, -0.03, f"{centroid:.1f}")
 
 
-def show_animation():
+def show_animation(n_sets):
     plt.ion()
     
-    (input_sets, output_sets), agg = init_fuzzy_system()
+    (input_sets, output_sets), agg = init_fuzzy_system(n_sets)
     (input_sets_x, input_sets_y), (output_sets_x, output_sets_y) = init_set_values(input_sets, output_sets)
     for x_val in range(2000, -1, -10):
         (agg_x, agg_y), infs = agg.aggregate(x_val)
@@ -91,8 +106,8 @@ def show_animation():
 
 # Für eine Value (Höhe) plotten.
 # Interessant: 200, 800, 1200, 1800
-def show_single_value(x_val):
-    (input_sets, output_sets), agg = init_fuzzy_system()
+def show_single_value(x_val, n_sets):
+    (input_sets, output_sets), agg = init_fuzzy_system(n_sets)
     (agg_x, agg_y), infs = agg.aggregate(x_val)
     centroid = fuzzy.Defuzzy().centroid(agg_x, agg_y)
 
@@ -121,9 +136,10 @@ class Plotting:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--value', type=int, required=False)
+    parser.add_argument('--n-sets', type=int, required=False, default=3, choices=[3, 5])
     args = parser.parse_args()
 
     if args.value: 
-        show_single_value(args.value)
+        show_single_value(args.value, args.n_sets)
     else:
-        show_animation()
+        show_animation(args.n_sets)
