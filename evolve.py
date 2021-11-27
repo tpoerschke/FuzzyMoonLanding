7933aab910027cf7855168619c477ba0b66da89f
Tim Poerschke <info@coder-tim.de>
2021-11-27 20:06:10 +0100

    Parallelisiert

--- evolve.py
+++ evolve.py
@@ -1,8 +1,10 @@
-import random, csv, time
+import os, random, csv, time, multiprocessing
 from deap import creator, base, tools
 
 import fuzzy
-from landing_game import game_state, game_tick, uhr, set_lever, reset_globals
+from landing_game import LandingGame
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 def play(individual):
     input_sets = [
@@ -20,19 +22,20 @@ def play(individual):
     agg = fuzzy.Aggregator(input_sets, output_sets, 100)
     defuzzy = fuzzy.Defuzzy()
 
-    game_state.game_ended = False
-    reset_globals()
-    while not game_state.game_ended:
-        uhr.tick(30)
-        game_tick(draw=False)
+    lg = LandingGame()
+    lg.game_state.game_ended = False
+    while not lg.game_state.game_ended:
+        lg.uhr.tick(30)
+        lg.game_tick(draw=False)
         # Aggregieren & Hebel einstellen
-        (x, y), _ = agg.aggregate(game_state.hoehe)
+        (x, y), _ = agg.aggregate(lg.game_state.hoehe)
         try:
             gas_percent = 1 - defuzzy.centroid(x, y) / 100
         except ZeroDivisionError: 
             gas_percent = 1
-        set_lever(gas_percent)
+        lg.set_lever(gas_percent)
         #print(f"Gas: {gas_percent:.2f} | Height: {game_state.hoehe:.2f} | Fuel: {game_state.fuel:.2f}")
+    return lg.game_state
 
 def gen_individual():
     mx_list = []
@@ -52,12 +55,11 @@ def gen_individual():
     return ind
 
 def evaluate(individual):
-    play(individual)
+    game_state = play(individual)
     if game_state.success:
         return abs(game_state.velocity),
     else:
         return abs(game_state.velocity) * 1000,
-
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
@@ -71,69 +73,77 @@ toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutUniformInt, low=0, up=[2000]*6 + [100]*6, indpb=0.05) # TODO: gewährleisten, dass x1 < x2 gilt
 toolbox.register("select", tools.selTournament, tournsize=3)
 
+def main():
+    N_PROCESSES = 10
+    pool = multiprocessing.Pool(N_PROCESSES,)
+    print(f"Anzahl an Prozessen: {N_PROCESSES}")
+    toolbox.register("map", pool.map)
 
-pop = toolbox.population(n=100)
-# Population evaluieren
-fitnesses = list(map(toolbox.evaluate, pop))
-for ind, fit in zip(pop, fitnesses):
-    ind.fitness.values = fit
-
-# CXPB:  Wahrscheinlichkeit, dass sich zwei Individuen paaren
-# MUTPB: Wahrscheinlichkeit, dass ein Individuum mutiert
-CXPB, MUTPB = 0.5, 0.2
-
-# Fitness-Werte extrahieren
-fits = [ind.fitness.values[0] for ind in pop]
-
-gen = 0
-# Lasset die Evolution beginnen
-# Ab 15m/s gilt eine Landung als erfolgreich, 
-# daher wird dann abgebrochen. Kann verringert
-# werden, wenn eine sanftere Landung 
-# erforderlich ist. Auch gamestate.success könnte
-# Anwendung finden, dann kann jedoch nicht die
-# Geschwindigkeit kontrolliert werden. 
-while min(fits) > 15 and gen < 1000:
-    # A new generation
-    gen += 1
-    print(f"-- Generation {gen} --")
-    # Nächste Generation auswählen
-    offspring = toolbox.select(pop, len(pop))
-    # Klonen zur Manipulation
-    offspring = list(map(toolbox.clone, offspring))
-    # Crossover
-    for child1, child2 in zip(offspring[::2], offspring[1::2]):
-        if random.random() < CXPB:
-            toolbox.mate(child1, child2)
-            del child1.fitness.values
-            del child2.fitness.values
-    # Mutation
-    for mutant in offspring:
-        if random.random() < MUTPB:
-            toolbox.mutate(mutant)
-            del mutant.fitness.values
-    # Manipulierte Individuen erneut evaluieren
-    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-    fitnesses = map(toolbox.evaluate, invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
+    pop = toolbox.population(n=100)
+    # Population evaluieren
+    print(f"-- Generation Genesis --")
+    fitnesses = list(toolbox.map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
-    # Poulation ersetzen
-    pop[:] = offspring
+
+    # CXPB:  Wahrscheinlichkeit, dass sich zwei Individuen paaren
+    # MUTPB: Wahrscheinlichkeit, dass ein Individuum mutiert
+    CXPB, MUTPB = 0.5, 0.2
+
     # Fitness-Werte extrahieren
     fits = [ind.fitness.values[0] for ind in pop]
-    # Statistiken
-    length = len(pop)
-    mean = sum(fits) / length
-    sum2 = sum(x*x for x in fits)
-    std = abs(sum2 / length - mean**2)**0.5
-    print("  Num %s" % length)
-    print("  Min %s" % min(fits))
-    print("  Max %s" % max(fits))
-    print("  Avg %s" % mean)
-    print("  Std %s" % std)
 
-# Individuuen der letzten Generation speichern
-with open(f'individuals_{time.time():.0f}.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    for ind, fit in zip(pop, fits):
-        writer.writerow([fit] + list(ind))
+    gen = 0
+    # Lasset die Evolution beginnen
+    # Ab 15m/s gilt eine Landung als erfolgreich, 
+    # daher wird dann abgebrochen. Kann verringert
+    # werden, wenn eine sanftere Landung 
+    # erforderlich ist. Auch gamestate.success könnte
+    # Anwendung finden, dann kann jedoch nicht die
+    # Geschwindigkeit kontrolliert werden. 
+    while min(fits) > 15 and gen < 1000:
+        gen += 1
+        print(f"-- Generation {gen} --")
+        # Nächste Generation auswählen
+        offspring = toolbox.select(pop, len(pop))
+        # Klonen zur Manipulation
+        offspring = list(map(toolbox.clone, offspring))
+        # Crossover
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+        # Mutation
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+        # Manipulierte Individuen erneut evaluieren
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        # Poulation ersetzen
+        pop[:] = offspring
+        # Fitness-Werte extrahieren
+        fits = [ind.fitness.values[0] for ind in pop]
+        # Statistiken
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2 / length - mean**2)**0.5
+        print("  Num %s" % length)
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+
+    # Individuuen der letzten Generation speichern
+    with open(f'individuals_{time.time():.0f}.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for ind, fit in zip(pop, fits):
+            writer.writerow([fit] + list(ind))
+
+if __name__ == "__main__":
+    main()
